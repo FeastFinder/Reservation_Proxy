4343d5348bcf967cc70fed80de007dd3dc29926d
Ryan Zhong <ryanzhong@Ryans-MacBook-Pro.local>
2019-09-24 19:10:48 -0700

    Added artillery stress test

--- server/proxy-server.js
+++ server/proxy-server.js
@@ -1,4 +1,7 @@
+require('newrelic');
+
 const express = require('express');
+const axios = require('axios');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -7,6 +10,15 @@ const port = 3000;
 
 
 app.use('/:id/', express.static('public'));
+
+app.get('/api/reservations/:id' , (req, res) => {
+  const { id } = req.params;
+  axios.get(`http://localhost:3002/api/reservations/${id}`)
+  .then( (data) => res.send(data.data) )
+  .catch(function (error) {
+    console.log(error);
+  })
+})
 
 
 app.listen(port, () => { console.log(`Proxy server listening on port ${port}`); });
